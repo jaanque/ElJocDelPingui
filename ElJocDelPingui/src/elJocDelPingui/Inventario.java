2e66a5d56d3d87119a58d5e7b91395a145b3f577
EricGalatanu <mfrancescharles@gmail.com>
2025-03-28 16:00:31 +0100

    ADD//
    GETTERS I SETTERS INVENTARIO

--- ElJocDelPingui/src/elJocDelPingui/Inventario.java
+++ ElJocDelPingui/src/elJocDelPingui/Inventario.java
@@ -1,5 +1,27 @@
 package elJocDelPingui;
 
-public class Inventario {
 
+//CLASSE INVENTARI 
+public class Inventario{
+
+	private int bolesNeu; 	//Boles de neu del Jugador.  
+	private int daus;	  	//Daus del Jugador. 
+	private int peixos;		//Peixos Jugador. 
+	
+//CONSTRUCTOR INVETARI 
+public Inventario() {
+	this.bolesNeu = 0 ;  //Boles de Inici.
+	this.daus = 0; 		//Daus Inici.										//COMENÇEM CADA INVENTARI AMB 0 OBJECTES DE CADA 
+	this.peixos = 0; 	//Peixos per inici.
+	}
+
+	//GETTERS 
+	public int getbolesNeu() {return bolesNeu;}
+	public int daus() {return daus;}
+	public int peixos() {return peixos;}
+	
+	//SETTERS 
+	public void setbolesNeu(int bolesNeu) {this.bolesNeu = bolesNeu;} 
+	public void setdaus(int daus) {this.daus = daus;}
+	public void setPeixos(int Peixos) {this.peixos = peixos;}  
 }
