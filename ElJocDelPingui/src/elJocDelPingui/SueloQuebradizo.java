babb035bac69e5325b016d20bb3b7fcf9e636a0c
Santi-Corrales <186623665+IvanMolinaAguayo@users.noreply.github.com>
2025-04-01 16:37:19 +0200

    ADD // SueloQuebradizo

--- ElJocDelPingui/src/elJocDelPingui/SueloQuebradizo.java
+++ ElJocDelPingui/src/elJocDelPingui/SueloQuebradizo.java
@@ -1,5 +1,28 @@
 package elJocDelPingui;
 
+//CLASE
 public class SueloQuebradizo {
+	
+	private int posicio;
+	private String Jugador;
+	
+	//CONSTRUCTOR CLASE 
+	public SueloQuebradizo (int posicio, String Jugador) {
+	
+		this.posicio = posicio;
+		this.Jugador = Jugador;
+		
+}
 
+	//GETTERS
+	
+		public int getposicio() {return posicio;}
+		
+		
+		
+	//SETTERS
+
+		public void setposicio (int posicio) {this.posicio = posicio;}
+	
+	
 }
