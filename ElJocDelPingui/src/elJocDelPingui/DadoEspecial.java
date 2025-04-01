5f95b9a8eb13ac44de16ac246016ee1541395142
EricGalatanu <mfrancescharles@gmail.com>
2025-04-01 16:35:20 +0200

    ADD//
    TIRAR DAU NORMAL I DAU ESPECIAL

--- ElJocDelPingui/src/elJocDelPingui/DadoEspecial.java
+++ ElJocDelPingui/src/elJocDelPingui/DadoEspecial.java
@@ -1,5 +1,20 @@
 package elJocDelPingui;
-
+import java.util.Random;
 public class DadoEspecial {
 
-}
+	
+	//DAU ESPECIAL A DE AVANÇAR DE 5 A 10 POSICIONS 
+	
+	 public static void tirarDauEspecial(String[] args) {
+		 Random random = new Random(); 
+		 
+		 System.out.println("TIRANDO  DADO... ");
+		 int resultatdau = random.nextInt(10)+5;
+		 
+		 System.out.println(resultatdau);
+		 
+		 
+	 }
+}	
+
+		
